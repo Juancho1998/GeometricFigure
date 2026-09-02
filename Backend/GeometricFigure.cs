0411e2b48d6fd3a79aa04495f0b0bca0da18f32d
Juan Daniel Retrepo Zapata <Juan-zap98@hotmail.com>
2026-09-02 13:28:06 -0500

    Add program and class Geometric figure and class Circle

---
+++ Backend/GeometricFigure.cs
@@ -0,0 +1,25 @@
+﻿namespace Backend;
+
+public abstract class GeometricFigure
+{
+
+    //Constructors
+    protected GeometricFigure(string name)
+    {
+        Name = name;
+    }
+
+    //Propierties
+
+    public string Name { get; set; }
+
+
+    //Public Methods
+
+    public abstract float GetArea();
+
+    public abstract float GetPerimeter();
+
+    public override string ToString() => $"{Name}\t\t => " + $"Area.....: {GetArea():N5}\t\t Perimeter.....: {GetPerimeter():N5}";
+
+}
---
+++ Frontend/Program.cs
@@ -0,0 +1,13 @@
+﻿using Backend;
+
+
+var circle = new Circle(name: nameof(Circle), r: 5);
+
+var figures = new List<GeometricFigure>
+{
+circle
+};
+foreach (var figure in figures)
+{
+    Console.WriteLine(figure);
+}
