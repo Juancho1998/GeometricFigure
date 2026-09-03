7e9c677a1f5c143b84ad9014962b3e40feb5f54f
Juan Daniel Retrepo Zapata <Juan-zap98@hotmail.com>
2026-09-02 19:59:22 -0500

    add class kite

---
+++ Backend/Kite.cs
@@ -0,0 +1,32 @@
+﻿namespace Backend;
+
+public class Kite : Rhombus
+{
+
+    //Fields
+    private int _b;
+    //Constructor
+    public Kite(string name, int a, int b, int d1, int d2) : base(name, a, d1, d2)
+    {
+        B = b;
+    }
+
+    //Propierties
+    public int B
+    {
+        get => _b;
+        set => _b = ValidateB(value);
+    }
+    //Public Methods
+    public override float GetArea() => (D1 * D2) / 2;
+    public override float GetPerimeter() => 2 * (A + B);
+    //Private Methods
+    private int ValidateB(int B)
+    {
+        if (B < 0)
+        {
+            throw new Exception($"The area: {B}, cannot be less zero.");
+        }
+        return B;
+    }
+}
