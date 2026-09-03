6da17e99c3dc1cb3b173ffe447a89d4930fecf82
Juan Daniel Retrepo Zapata <Juan-zap98@hotmail.com>
2026-09-02 21:01:55 -0500

    add parallelogram class

---
+++ Backend/Parallelogram.cs
@@ -0,0 +1,35 @@
+﻿namespace Backend;
+
+public class Parallelogram : Rectangle
+{
+    //Fields
+    private double _h;
+    //Constructors
+    public Parallelogram(string name, double a, double b, double h) : base(name, a, b)
+    {
+        H = h;
+    }
+
+    public double H
+    {
+        get => _h;
+        set => _h = ValidateH(value);
+    }
+
+    //Propierties
+    //Public Methods
+    public override double GetArea() => B * H;
+
+    public override double GetPerimeter() => 2 * (A + B);
+    //Private Methods
+    private double ValidateH(double H)
+    {
+        if (H <= 0)
+        {
+            throw new Exception($"The area: {H}, cannot be less zero.");
+        }
+        return H;
+    }
+
+
+}
