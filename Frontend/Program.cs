8a951956e72f9d0d45cc40e882cb9cb4aee18cc3
Juan Daniel Retrepo Zapata <Juan-zap98@hotmail.com>
2026-09-08 08:30:43 -0500

    correction print figure kite

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -12,7 +12,7 @@ var trapeze = new Trapeze(name: nameof(Trapeze), a: 10, b: 20, c: 30, d: 40, h: 
 
 var figures = new List<GeometricFigure>
 {
-circle, square, rhombus, rectangle,parallelogram, triangle,trapeze
+circle, square, rhombus, kite, rectangle,parallelogram, triangle,trapeze
 };
 foreach (var figure in figures)
 {
